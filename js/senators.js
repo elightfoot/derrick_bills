95cfe252a8dcb64c12d0736b407d0df8f41b60b8
dbills777 <dbills777@gmail.com>
2019-11-08 20:28:49 -0700

    added senators to portfolio page

--- js/senators.js
+++ js/senators.js
@@ -21,14 +21,47 @@ function populateDom(senator_array) {
         let cardImage = document.createElement('div')
         cardImage.setAttribute('class', 'card-image')
         let figure = document.createElement('figure')
-        figure.setAttribute('class', 'image is-4by3')
+        figure.setAttribute('class', 'image ')
         let figureImage = document.createElement('img')
-        figureImage.src = 'https://bulma.io/images/placeholders/1280x960.png'
+        figureImage.src = `https://www.congress.gov/img/member/${senator.id.toLowerCase()}_200.jpg`
         figureImage.alt = 'Placeholder image'
 
         figure.appendChild(figureImage)
         cardImage.appendChild(figure)
         card.appendChild(cardImage)
+        //added this after creating the cardContent function
+        card.appendChild(cardContent(senator))
         container.appendChild(card)
     })
+}
+function cardContent(senator) {
+    let cardContent = document.createElement('div')
+    cardContent.setAttribute('class', 'card-content')
+    let media = document.createElement('div')
+    media.setAttribute('class', 'media')
+    let mediaLeft = document.createElement('div')
+    mediaLeft.setAttribute('class', 'media-left')
+    let figure = document.createElement('figure')
+    figure.setAttribute('class', 'image is-48x48')
+    let img = document.createElement('img')
+    img.src = `https://bulma.io/images/placeholders/96x96.png`
+    img.alt = 'Placeholder image'
+    let mediaContent = document.createElement('div')
+    mediaContent.setAttribute('class', 'media-content')
+    let titleP = document.createElement('p')
+    titleP.setAttribute('class', 'title is-4')
+    titleP.textContent = `${senator.first_name} ${senator.last_name}`
+    let subTitleP = document.createElement('p')
+    subTitleP.setAttribute('class', 'subtitle is-6')
+    subTitleP.textContent = `Home state: ${senator.state}`
+
+    mediaContent.appendChild(titleP)
+    mediaContent.appendChild(subTitleP)
+    figure.appendChild(img)
+    mediaLeft.appendChild(figure)
+    media.appendChild(mediaLeft)
+    media.appendChild(mediaContent)
+    cardContent.appendChild(media)
+    return cardContent
+
 }
